6d3d6112417560e92eafe64adb100f6c6aa13c30
Moussa Boussi <Moussaboussi@gmail.com>
2019-02-05 12:34:05 -0500

    changed click to document on click

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 "use strict";
 $(document).ready(function () {
     let selectedTable;
-    $(".available").on("click", function (e) {
+    $(document).on("click", ".available", function (e) {
         $("form").css("display", "flex");
         selectedTable = e.target
         $(".tablenum").text(`Table Number: ${$(selectedTable).text()}`)
     })
 
-    $(".close").on("click", function () {
+    $(document).on("click", ".close", function () {
         $("form").hide()
     })
 
-    $(".btn_submit").on("click", function () {
+    $(document).on("click", ".btn_submit", function () {
         $("form").hide()
     })
 
