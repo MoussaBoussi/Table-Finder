df28ad2a30aea9f8bd20ff85b33b51b6e95b5f2a
Moussa Boussi <Moussaboussi@gmail.com>
2019-02-05 17:01:11 -0500

    added functionality

--- script.js
+++ script.js
@@ -17,6 +17,21 @@ $(document).ready(function () {
 
     $(document).on("click", ".btn_submit", function (e) {
         $(selectedTable).removeClass("available").addClass("reserved")
+        $(selectedTable).attr("name", $(".name").val())
+        $(selectedTable).attr("size", $(".numGuests").val())
         $("form").hide()
+        $(selectedTable).append(`
+        <section class = "popup">
+            <p>Name:  ${$(selectedTable).attr("name")}</p>
+            <p>Size of Party: ${$(selectedTable).attr("size")}</p>
+        </section>`)
     })
+
+    $(document).on("mouseenter", ".reserved", function (event) {
+        $(".popup").css("display", "block")
+      }).on("mouseleave", ".reserved", (event) => {
+        $(".popup").css("display", "none")
+      });
+
+
 });
